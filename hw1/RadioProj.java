9816b956ca5225400cf189ddc09a2b5d3eae1e0b
Austin Walker <awalker4@umassd.edu>
2014-09-26 17:13:07 +0000

    Fix freqBottom issue

--- hw1/RadioProj.java
+++ hw1/RadioProj.java
@@ -1,10 +1,10 @@
 
-/***********************************************************
- * Program Assignment #1 (CIS 461, Fall 2014)              *
- * Submitted By: Your names here !!!                        *
- * SID: Your new SIDs starting with "00"                    *
- * Date: Your submission date                              *
- ***********************************************************/
+/****************************************************************
+ * Program Assignment #1 (CIS 461, Fall 2014)                   *
+ * Submitted By: Harry Low, Mackinley Trudeau,  Austin Walker   *
+ * SID: Your new SIDs starting with "00"                        *
+ * Date: 09/26/2014                                             *
+ ****************************************************************/
 
 /**
  * CIS 461: Formal Methods for Software Engineering
@@ -14,7 +14,6 @@
  * @author Haiping Xu
  * Revised on Sept. 15, 2014
  **/
-package radioproj;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
@@ -27,28 +26,28 @@ import javax.swing.JPanel;
 import java.text.DecimalFormat;
 
 public class RadioProj implements Runnable, ActionListener {
-	private final static int freqTop = 108;
-	private final static int freqBottom = 88;
-	private float frequency = freqTop;
-        private float fre=0;
-	private double[] lockFrequency = {105.9, 101.8, 98.5, 95.6, 92.1,88.0};
-	private DisplayPanel display;
-	private JButton on;
-	private JButton off;
-	private JButton scan;
-	private JButton reset;
+    private final static int freqTop = 108;
+    private final static int freqBottom = 88;
+    private float frequency = freqTop;
+    private float fre=0;
+    private double[] lockFrequency = {105.9, 101.8, 98.5, 95.6, 92.1};
+    private DisplayPanel display;
+    private JButton on;
+    private JButton off;
+    private JButton scan;
+    private JButton reset;
 
-	private volatile Thread searchChannel;
+    private volatile Thread searchChannel;
 
-	public void init() {
-		JFrame frame = new JFrame("FM Radio");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container pane = frame.getContentPane();
+    public void init() {
+        JFrame frame = new JFrame("FM Radio");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container pane = frame.getContentPane();
 
- 		display=new DisplayPanel("FM Radio");
-		pane.add(display, BorderLayout.CENTER);
+        display=new DisplayPanel("FM Radio");
+        pane.add(display, BorderLayout.CENTER);
 
-		JPanel p = new JPanel();
+        JPanel p = new JPanel();
         on = new JButton("on");
         on.addActionListener(this);
         p.add(on);
@@ -65,85 +64,86 @@ public class RadioProj implements Runnable, ActionListener {
         reset.addActionListener(this);
         p.add(reset);
 
-		pane.add(p, BorderLayout.SOUTH);
+        pane.add(p, BorderLayout.SOUTH);
 
-		frame.pack();
-		frame.setSize(350, 300);
-		frame.setVisible(true);
-	}
+        frame.pack();
+        frame.setSize(350, 300);
+        frame.setVisible(true);
+    }
 
-	public static void main(String[] argv) {
-		RadioProj radio = new RadioProj();
-		radio.init();
-	}
+    public static void main(String[] argv) {
+        RadioProj radio = new RadioProj();
+        radio.init();
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getActionCommand().equals("on")) {
-			display.turnOn();
-			frequency =  freqTop;
-			display.setValue(frequency);
-                        on.setEnabled(false);
-		}
-		else if (e.getActionCommand().equals("off")) {
-			display.turnOff();
-			searchChannel = null;
-                        on.setEnabled(true);
-                        scan.setEnabled(true);
-		}
-		else if (e.getActionCommand().equals("scan")) {
-			if (display.isOn()) scanning();
-		}
-		else if (e.getActionCommand().equals("reset")) {
-			if (display.isOn()) reset();
-		};
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getActionCommand().equals("on")) {
+            display.turnOn();
+            frequency =  freqTop;
+            display.setValue(frequency);
+            on.setEnabled(false);
+        }
+        else if (e.getActionCommand().equals("off")) {
+            display.turnOff();
+            searchChannel = null;
+            on.setEnabled(true);
+            scan.setEnabled(true);
+        }
+        else if (e.getActionCommand().equals("scan")) {
+            if (display.isOn()) scanning();
+        }
+        else if (e.getActionCommand().equals("reset")) {
+            if (display.isOn()) reset();
+        };
+    }
 
 
     //  ====>>>>> Complete the methods below this line! <<<<<====
 
-	private void scanning() {
+    private void scanning() {
         reset.setEnabled(false); 
         off.setEnabled(false);
         searchChannel = new Thread(this);
         searchChannel.start();
-	}
+    }
 
-	private void reset() {
+    private void reset() {
         frequency = freqTop;
         display.setValue(frequency);
         scan.setEnabled(true);
-	}
+    }
 
-	@Override
-	public void run() {
-            if(frequency>88.1){
+    @Override
+    public void run() {
 
-                while(true) {
-                    frequency = frequency-0.1f;
-                    display.setValue(frequency);
-                        // Pause execution so frequency changes can be seen better
-                    try {
-                        Thread.sleep(40);
-                    } catch(Exception e) {}
+        while(frequency>=freqBottom) {
+            frequency = frequency-0.1f;
+            display.setValue(frequency);
 
-                    if((int)Math.ceil((double)frequency) == freqBottom) {
-                        reset.setEnabled(true); 
-                        off.setEnabled(true);
-                        scan.setEnabled(false);
-                        return;
-                    }
+            // Pause execution so frequency changes can be seen better
+            try {
+                //Thread.sleep(40);
+            } catch(Exception e) {}
 
-                    for(double i:lockFrequency) {
-                        if(String.format("%.1f",frequency).equals(Double.toString(i))) {
-                            reset.setEnabled(true); 
-                            off.setEnabled(true);
-                            return;
-                        }
-                    }
+            // Check if current station is in the array
+            for(double i:lockFrequency) {
+                if(String.format("%.1f",frequency).equals(Double.toString(i))) {
+                    reset.setEnabled(true); 
+                    off.setEnabled(true);
+                    return;
                 }
-            
+            }
+
+            if((int)Math.floor((double)frequency) == freqBottom-1) {
+                display.setValue(freqBottom);
+                reset.setEnabled(true); 
+                off.setEnabled(true);
+                scan.setEnabled(false);
+                return;
+            }
+
         }
     }
 }
