b761f3a6381708f0a06372f1daf86af428b135e6
Austin Walker <awalker4@umassd.edu>
2014-11-16 15:45:45 -0500

    Clean up formatting

---
+++ hw4/DiningSavages.java
@@ -0,0 +1,14 @@
+public class DiningSavages {
+
+    // private final int MaxServings;
+    // private final int NumberOfSavages;
+
+    public static void main(String[] args) {
+        System.out.println("Goodbye world");
+    }    
+    
+    private class Cook implements Runnable {
+        public void run() {
+        }
+    }
+}
